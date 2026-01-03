fd85e5f17ecb4f09e7b68ea169414b786389f19d
Maykhav <ahmedcheikh3266@gmail.com>
2026-01-03 20:25:53 +0100

    Add setup.py for MeArm model package
    Added setup configuration for the MeArm model package including dependencies, data files, and entry points.

--- similation_mearm/src/setup.py
+++ similation_mearm/src/setup.py
@@ -1,1 +1,38 @@
+from setuptools import setup
 
+package_name = 'mearm_model'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdf', [
+            'urdf/robot.urdf.xacro',
+            'urdf/ball.urdf.xacro',
+            'urdf/robot.urdf']),
+        ('share/' + package_name + '/config', [
+            'config/joint_positions_sets.yaml']),
+        ('share/' + package_name + '/rviz', [
+            'rviz/urdf.rviz']),
+        ('share/' + package_name + '/launch', [
+            'launch/display.launch.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ahmed',
+    maintainer_email='ahmed@example.com',
+    description='MeArm model package for ROS2 with URDF, RViz and Python nodes',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ball_publisher = mearm_model.ball_publisher:main',
+            'filter = mearm_model.filter:main',
+            'move = mearm_model.move:main',
+        ],
+    },
+)
